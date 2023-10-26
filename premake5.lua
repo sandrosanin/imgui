eea7ba2b2e2ace08bed13ef5ff6b7b5e59e9e459
sandrosanin <26463637+sandrosanin@users.noreply.github.com>
2023-10-26 17:09:42 +0200

    updated premake & gitignore

--- premake5.lua
+++ premake5.lua
@@ -9,14 +9,17 @@ project "ImGui"
 	{
 		"imconfig.h",
 		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
+        "imgui_internal.h",
+        "imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+
+		"imgui.cpp",
+        "imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+
 	}
 
 	filter "system:windows"
